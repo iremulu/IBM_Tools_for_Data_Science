eb77b28ce972f41be41405f41313c318174fe8da
irem <78342399+iremulu@users.noreply.github.com>
2022-12-21 20:26:03 +0300

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# <h1>My Jupyter Notebook on IBM Watson Studio<h1>
+
+# <b>Sevim Irem Ulu </b> \
+# Specialist Motor Control Engineer
+
+# <i> I am interested in data science due to its necessity building machine learning models.  </i> 
+# 
+# 
+
+# <h3>The code in the Cell 5 has two variable a and b that have certain values, by adding them up we can calculate c value, which is 5. <h3>
+
+# In[16]:
+
+
+a = 2
+b = 3
+c = a + b
+print(c)
+
+
+# <blockquote>
+#     <h2>block quoted header</h2>
+# 
+# </blockquote>
+# 
+# * * *
+# horizontal rule 1
+# 
+# ***
+# horizontal rule 2
+# 
+# *****
+# horizontal rule 3
+# 
+# 
+# * Bulleted list item 1
+# * Bulleted list item 2
+# * Bulleted list item 3
+# 
+# > block quoted item
+# 
+
+# In[ ]:
+
+
+
+
